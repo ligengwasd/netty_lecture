4b4b1dc28ca9fc59a5f7d496fa78f4b6e8c09f2d
李赓 <ligeng@enmonster.com>
2019-07-15 01:15:25 +0800

    netty 微信

---
+++ src/main/java/com/weixin/NettyServer.java
@@ -0,0 +1,50 @@
+package com.weixin;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+/**
+ * @Author ligeng
+ * @Date 19/7/15
+ * @Time 上午1:08
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                .childAttr(AttributeKey.newInstance("clientKey"), "clientValue")
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        System.out.println("服务端启动中");
+                    }
+                })
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true);
+
+        serverBootstrap.bind(8000).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("端口绑定成功!");
+                } else {
+                    System.err.println("端口绑定失败!");
+                }
+            }
+        });
+    }
+}
